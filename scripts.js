93aa325daeb851999138856793b47edfda74c3a3
husnu mulyadi <mulyadihusnu@gmail.com>
2024-05-17 13:44:38 +0700

    add style and js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.addEventListener('DOMContentLoaded', function() {
+    // Example of a simple JavaScript functionality
+    console.log('Portfolio loaded successfully.');
+});
